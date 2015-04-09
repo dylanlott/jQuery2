21a82c2604b9389f317a0102d2468daa3c9d9551
dylanlott <lott.dylan@gmail.com>
2015-04-09 14:35:41 -0600

    add bootstrap and script

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+	var listo = []; 
+
+	var Task = function(task){
+		this.task = task; //task constructor -- remember this format 
+		this.id = 'new'; 
+	}	
+
+
+	var addTask = function(task){
+		if(task) {
+			task = new Task(task); 
+			listo.push(task); 
+
+			$('#newItemInput').val(); 
+			$('#newList').append('<a href="#" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon-arrow-right"></span></li></a>');
+			
+		}
+	}
+
+	$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear'); 
+
+
+	//opens form 
+	$('#newListItem').on('click', function() {
+		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
+	}); 
+
+	//closes form 
+	$('#cancel').on('click', function(e) {
+		e.preventDefault(); 
+		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
+	})
+
+})
