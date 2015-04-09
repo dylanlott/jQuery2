0bb4b9afb33d21a628fa40258414dd1f08a33bf2
dylanlott <lott.dylan@gmail.com>
2015-04-09 15:14:14 -0600

    all code added

--- app.js
+++ app.js
@@ -33,4 +33,43 @@ $(document).ready(function(){
 		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
 	})
 
-})
+	var advanceTask = function(task) {
+	    var modified = task.innerText.trim()
+	    for (var i = 0; i < listo.length; i++) {
+	        if (listo[i].task === modified) {
+	            if (listo[i].id === 'new') {
+	                listo[i].id = 'inProgress';
+	            } else if (listo[i].id === 'inProgress') {
+	                listo[i].id = 'archived';
+	            } else {
+	                listo.splice(i, 1);
+	            }
+	            break;
+	        }
+	    }
+	    task.remove();
+	};﻿
+
+	$(document).on('click', '#item', function(e) { //call document hook to let document know we're adding things to it. 
+		e.preventDefault(); 
+		var task = this; 
+		advanceTask(task); 
+		this.id = 'inProgress';
+		$('#currentList').append(this.outerHTML); 
+	})
+
+	$(document).on('click', '#inProgress', function(e) {
+		e.preventDefault(); //makes sure HTML elements render without default properties 
+		var task = this; 
+		task.id = "archived"; 
+		var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove'); 
+		advanceTask(task); 
+		$('#archivedList').append(changeIcon); 
+	})
+
+	$(document).on('click', '#archived', function(e) {
+		e.preventDefault(); 
+		var task = this; 
+		advanceTask(task); 
+	})
+}
