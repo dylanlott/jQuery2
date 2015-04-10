357c81d5deefbdffa74311af94c5d36e8aaf4b54
dylanlott <lott.dylan@gmail.com>
2015-04-09 20:47:16 -0600

    updated javascript reviews

--- app.js
+++ app.js
@@ -3,35 +3,22 @@ $(document).ready(function(){
 	var listo = []; 
 
 	var Task = function(task){
-		this.task = task; //task constructor -- remember this format 
+		this.task = task; //task constructor 
 		this.id = 'new'; 
 	}	
 
+	var addTask = function(task) {
+    if(task) {
+        task = new Task(task);
+        listo.push(task);
+        console.log("task added" + task.task); 
+        $('#newItemInput').val('');
 
-	var addTask = function(task){
-		if(task) {
-			task = new Task(task); 
-			listo.push(task); 
+        $('#newList').append('<a href="#finish" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
 
-			$('#newItemInput').val(); 
-			$('#newList').append('<a href="#" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon-arrow-right"></span></li></a>');
-			
-		}
-	}
-
-	//$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear'); //this shit is broken as fuck 
-
-
-	//opens form 
-	$('#newListItem').on('click', function() {
-		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
-	}); 
-
-	//closes form 
-	$('#cancel').on('click', function(e) {
-		e.preventDefault(); 
-		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
-	})
+    	}
+    //$('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+	};
 
 	var advanceTask = function(task) {
 	    var modified = task.innerText.trim()
@@ -48,7 +35,8 @@ $(document).ready(function(){
 	        }
 	    }
 	    task.remove();
-	};﻿
+	    console.log("removed"); 
+	};
 
 	$(document).on('click', '#item', function(e) { //call document hook to let document know we're adding things to it. 
 		e.preventDefault(); 
@@ -72,4 +60,4 @@ $(document).ready(function(){
 		var task = this; 
 		advanceTask(task); 
 	})
-}
+});
